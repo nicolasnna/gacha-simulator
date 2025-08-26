bb116701aa515cfb9afe5047f64c22d6ae1b70e4
Nicolás Norambuena <nicolas.norambuenaa@gmail.com>
2025-08-26 12:02:53 -0400

    feat: :sparkles: back: Added create role dto. Added create Role & implents jwt/access to role controller.

--- backend/src/roles/dto/create-role.dto.ts
+++ backend/src/roles/dto/create-role.dto.ts
@@ -1,1 +1,10 @@
-export class CreateRoleDto {}
+import { IsOptional, IsString } from 'class-validator'
+import { GrantsArrayDto } from './grants-role.dto'
+
+export class CreateRoleDto extends GrantsArrayDto {
+  @IsString()
+  key!: string
+
+  @IsOptional()
+  label: string
+}
