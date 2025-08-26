0d20132976d6d6b0e918f902a7e1ab537101cbf1
Nicolás Norambuena <nicolas.norambuenaa@gmail.com>
2025-08-26 12:14:56 -0400

    feat: :sparkles: back: Added Jwt & Access guard on character & users controller.

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -6,43 +6,56 @@ import {
   Param,
   Patch,
   Post,
-  Query
+  Query,
+  UseGuards
 } from '@nestjs/common'
 import { UpdateUserInfoDto } from './dto/update-user-info.dto'
 import { UsersService } from './users.service'
 import { CreateUserDto } from './dto/create-user.dto'
+import { JwtAuthGuard } from '@/auth/guards/jwt-auth.guard'
+import { AccessGuard } from '@access/access.guard'
+import { Action, ModuleResource } from '@access/access.decorator'
+import { ActionKeyEnum, ModuleKeyEnum } from '@common/enums'
 
+@UseGuards(JwtAuthGuard, AccessGuard)
+@ModuleResource(ModuleKeyEnum.Users)
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @Action(ActionKeyEnum.CREATE)
   @Post()
   create(@Body() dataUser: CreateUserDto) {
     return this.usersService.create(dataUser)
   }
 
+  @Action(ActionKeyEnum.READ)
   @Get()
   findAll(@Query('page') page: number, @Query('limit') limit: number) {
     return this.usersService.findAll(page, limit)
   }
 
+  @Action(ActionKeyEnum.READ)
   @Get(':id')
   findById(@Param('id') id: string) {
     return this.usersService.findById(id)
   }
 
+  @Action(ActionKeyEnum.UPDATE)
   @Patch(':id')
   updateInfo(@Param('id') id: string, @Body() userInfo: UpdateUserInfoDto) {
     return this.usersService.updateInfo(id, userInfo)
   }
 
-  @Delete(':id')
-  deactivateById(@Param('id') id: string) {
-    return this.usersService.deactivateById(id)
-  }
-
+  @Action(ActionKeyEnum.UPDATE)
   @Patch(':id/activate')
   activateById(@Param('id') id: string) {
     return this.usersService.activateById(id)
+  }
+
+  @Action(ActionKeyEnum.DELETE)
+  @Delete(':id')
+  deactivateById(@Param('id') id: string) {
+    return this.usersService.deactivateById(id)
   }
 }
--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -3,10 +3,12 @@ import { UsersService } from './users.service'
 import { UsersController } from './users.controller'
 import { MongooseModule } from '@nestjs/mongoose'
 import { User, UserSchema } from '@common/schemas'
+import { RolesModule } from '@/roles/roles.module'
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    RolesModule
   ],
   controllers: [UsersController],
   providers: [UsersService],
